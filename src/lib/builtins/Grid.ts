8932cf1f4d370f3a30db47491b86d099d9786b88
terrygonguet <terry@gonguet.com>
2022-01-24 00:15:12 +0100

    More grid options

--- src/lib/builtins/Grid.ts
+++ src/lib/builtins/Grid.ts
@@ -5,6 +5,8 @@ export function initState() {
 		cols: 6,
 		rows: 2,
 		gap: 1,
+		grow: true,
+		placeholders: false,
 	}
 }
 
@@ -18,6 +20,18 @@ export const editorConfig: EditorConfig = [
 		min: 0,
 		step: 0.25,
 		info: "The size of the gutter between items.",
+	},
+	{
+		type: "boolean",
+		prop: "grow",
+		label: "Grow:",
+		info: "Grow to take all available space when child of <code>Container</code>.",
+	},
+	{
+		type: "boolean",
+		prop: "placeholders",
+		label: "Visualize slots:",
+		info: "Useful to see what the grid may look like once full.",
 	},
 ]
 
@@ -36,8 +50,17 @@ export async function render(
 	el.style.gap = state.gap + "rem"
 	el.style.gridTemplateColumns = `repeat(${state.cols}, 1fr)`
 	el.style.gridTemplateRows = `repeat(${state.rows}, 1fr)`
+	if (state.grow) el.style.flexGrow = "1"
 
 	children.forEach(child => el.appendChild(child))
+
+	if (state.placeholders) {
+		for (let i = 0; i < state.cols * state.rows - children.length; i++) {
+			const div = document.createElement("div")
+			div.style.border = "1px solid var(--color-accent)"
+			el.appendChild(div)
+		}
+	}
 
 	return el
 }
