62f17c3b2527cb2d403465bac09c23f4b3002bc8
terrygonguet <terry@gonguet.com>
2022-01-23 23:44:09 +0100

    Better tree

--- src/lib/components/ComponentDefinitionTree.svelte
+++ src/lib/components/ComponentDefinitionTree.svelte
@@ -6,30 +6,44 @@
 	export let depth = 0
 	export let definition: ComponentDefinition
 	export let selected: string
+	export let lastInList = false
 
 	const emit = createEventDispatcher<{ select: string }>()
 
 	$: builtinName =
 		definition.type == "Custom" ? "Custom" : builtins[definition.type].name
 	$: name = (definition.name || builtinName) ?? definition.type
+	$: prefixStart = depth > 0 ? "│".repeat(depth - 1) : ""
+	$: prefixSplit = lastInList ? "└" : "├"
+	$: prefixEnd = definition.children?.length ? "┬" : "─"
+	$: prefix = depth > 0 ? prefixStart + prefixSplit + prefixEnd : ""
+	$: lastChild = definition.children?.at(-1)
 </script>
 
-<label style="--depth:{depth}" class:selected={selected == definition.id}>
+<label class:selected={selected == definition.id}>
 	<input
 		type="radio"
 		value={definition.id}
 		name="component"
 		on:change={() => emit("select", definition.id)}
-	/>{name}
+	/>{prefix}{name}
 </label>
 {#each definition.children ?? [] as child}
-	<svelte:self depth={depth + 1} definition={child} {selected} on:select />
+	<svelte:self
+		depth={depth + 1}
+		definition={child}
+		{selected}
+		lastInList={child == lastChild && !child.children?.length}
+		on:select
+	/>
 {/each}
 
 <style>
 	label {
-		padding-left: calc(var(--depth, 0) * 0.7rem);
 		cursor: pointer;
+		font-family: "Courier New", Courier, monospace;
+		line-height: 1.1;
+		font-size: 1.1rem;
 	}
 	input[type="radio"] {
 		appearance: none;
